8e24482f306eedc9fdac30cb29eb6fa87b8f5959
AleksPaul <paulouskaya.aliaksandra@gmail.com>
2022-06-16 14:32:41 +0200

    Added counter on the pages: book, buy

--- bookshop/views.py
+++ bookshop/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Author, Book
 
 
 def index(request):
+    visits = request.session.get('visits', 0)
+    request.session['visits'] = visits + 1
+    books_amount = Book.objects.count()
+    author_amount = Author.objects.count()
     books = Book.objects.all()
-    return render(request, 'index.html', {'books': books})
+    return render(request, 'index.html', {'books': books,
+                                          'visits': visits, 
+                                          'books_amount': books_amount,
+                                          'author_amount': author_amount })
 
 
 def book(request, id):
     book = Book.objects.get(id = id)
-    return render(request, 'view.html', {'book': book})
+
+    visits = request.session.get('visits', 0)
+    request.session['visits'] = visits
+    books_amount = Book.objects.count()
+    author_amount = Author.objects.count()
+
+
+    return render(request, 'view.html', {'book': book, 'visits': visits,'books_amount': books_amount,'author_amount': author_amount })
 
 def buy(request, id):
     book = Book.objects.get(id = id)
-    return render(request, 'buy.html', {'book': book})
+
+    visits = request.session.get('visits', 0)
+    request.session['visits'] = visits
+    books_amount = Book.objects.count()
+    author_amount = Author.objects.count()
+
+    return render(request, 'buy.html', {'book': book, 'visits': visits,'books_amount': books_amount,'author_amount': author_amount})
